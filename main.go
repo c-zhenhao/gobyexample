ac0592e66029406198adcb9698354f4412e8be71
czhenhao <czhenhao@hotmail.com>
2022-09-11 15:45:27 +0800

    created endpoint to populate, GET all products and filter by title

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -35,9 +36,10 @@ func main() {
 
 	app.Use(cors.New())
 
-	// app.Get("/", func(c *fiber.Ctx) error {
-	// 	return c.SendString("Hello, World 👋!")
-	// })
+	// test endpoint
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
 
 	// endpoint to seed the database
 	app.Post("/api/products/populate", func(c *fiber.Ctx) error {
@@ -57,6 +59,56 @@ func main() {
 		return c.JSON(fiber.Map{
 			"message": "success",
 		})
+	})
+
+	app.Get("/api/products/frontend", func(c *fiber.Ctx) error {
+		collection := db.Collection("products")
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+		// create a product slice
+		var products []Product
+
+		cursor, _ := collection.Find(ctx, bson.M{})
+		defer cursor.Close(ctx)
+
+		for cursor.Next(ctx) {
+			var product Product
+			cursor.Decode(&product)
+			products = append(products, product)
+		}
+
+		return c.JSON(products)
+	})
+
+	app.Get("/api/products/backend", func(c *fiber.Ctx) error {
+		collection := db.Collection("products")
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+		var products []Product
+
+		filter := bson.M{}
+
+		if s := c.Query("search"); s != "" {
+			filter = bson.M{
+				"title": bson.M{
+					"$regex": primitive.Regex{
+						Pattern: s,
+						Options: "i",
+					},
+				},
+			}
+		}
+
+		cursor, _ := collection.Find(ctx, filter)
+		defer cursor.Close(ctx)
+
+		for cursor.Next(ctx) {
+			var product Product
+			cursor.Decode(&product)
+			products = append(products, product)
+		}
+
+		return c.JSON(products)
 	})
 
 	app.Listen(":8000")
